dd4f856812bf61a24ab1f35ce34d68100c2b1df7
Niklas Borson <neborson@outlook.com>
2017-10-04 12:22:59 -0700

    Add HelloWorld program with sample code. (#4)

---
+++ NiklasB/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uncomment one of the following lines.
+
+            FlowControl.Run();
+
+            // GuessingGame.Run();
+
+            // BinarySearch.Run();
+
+            // TextAdventure.Run();
+        }
+    }
+}
