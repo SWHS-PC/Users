32fa121063963372dd391cac29e479e8eec65629
Niklas Borson <neborson@outlook.com>
2017-12-14 15:25:41 -0800

    Option to write to a file

--- NiklasB/TimesTable/Program.cs
+++ NiklasB/TimesTable/Program.cs
@@ -7,15 +7,38 @@ namespace TimesTable
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                // No command line argument, so write to the console.
+                WriteTables(Console.Out);
+            }
+            else
+            {
+                // At least one command line argument. Assume the first
+                // argument is a file name and create a stream writer for it.
+                using (var output = new System.IO.StreamWriter(args[0]))
+                {
+                    // Write the tables to the file.
+                    WriteTables(output);
+                }
+            }
+        }
+
+        static void WriteTables(TextWriter output)
+        {
             // Write a decimal multiplication table (decimal format string and radix 10).
-            WriteTimesTable("{0,4}", 10, Console.Out);
+            WriteTimesTable("{0,4}", 10, output);
 
             // Write a hexadecimal multiplication table (hexadecimal format string and radix 16).
-            WriteTimesTable("{0,4:x}", 16, Console.Out);
+            WriteTimesTable("{0,4:x}", 16, output);
         }
 
 
-        static void WriteTimesTable(string formatString, int radix, TextWriter output)
+        static void WriteTimesTable(
+            string formatString, 
+            int radix, 
+            TextWriter output
+            )
         {
             // Outer loop executes once for each row.
             for (int row = 1; row < radix; row++)
