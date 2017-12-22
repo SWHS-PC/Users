d7f71e87146e5f327d80a61a49ede4f35d17cecc
Zaldimmar <PyroFire12@gmail.com>
2017-12-21 16:03:53 -0800

    Texas Hold'em and textAdven

---
+++ TexasHoldem/TexasHoldem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kitchen = new room("Kitchen");
+            kitchen.items.Add(new item("Spatula"));
+            var hallway = new room("Hallway");
+
+            hallway.North = kitchen;
+            hallway.South = hallway;
+
+            kitchen.Describe();
+
+            Console.ReadKey();
+
+        }
+    }
+}
