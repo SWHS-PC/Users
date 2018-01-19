4de97e86a6ff64db11f032b5d071cfaa805e0151
Zaldimmar <PyroFire12@gmail.com>
2018-01-18 16:11:13 -0800

    TextAdven

---
+++ TextAdven/TextAdven/Link.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdven
+{
+    class Link
+    {
+        public Link(string text, page destination)
+        {
+            Text = text;
+            Destination = destination;
+        }
+
+        public string Text { get; }
+        public page Destination { get; }
+
+    }
+}
