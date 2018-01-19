37ac79b1be4271be9e9bbade4b50938c686645a9
Niklas Borson <neborson@outlook.com>
2018-01-18 20:39:24 -0800

    Add option to repeat game, quit key, and comments.

--- NiklasB/Adventure/Program.cs
+++ NiklasB/Adventure/Program.cs
@@ -9,56 +9,107 @@ namespace Adventure
     {
         static void Main(string[] args)
         {
+            // Make sure a story file is specified on the command line.
             if (args.Length != 1)
             {
-                Console.WriteLine("Usage: Adventure <fileName.txt>");
+                Console.WriteLine("Usage: Adventure <storyFile.txt>");
                 return;
             }
 
-            var currentPage = LoadPages(args[0]);
-            if (currentPage == null)
+            // Load the story file, returning if it fails.
+            var startPage = LoadPages(args[0]);
+            if (startPage == null)
+                return;
+
+            // Play the game repeatedly until the user quits or says no.
+            while (PlayGame(startPage))
             {
-                return;
-            }
+                Console.WriteLine("THE END\n\nDo you want to play again? [yn] ");
 
+                if (!AskYesNo())
+                    break;
+
+                Console.WriteLine("\n------\n");
+            }
+        }
+
+        static bool PlayGame(Page startPage)
+        {
+            var currentPage = startPage;
+
+            // Loop until we reach and ending page (one with no links).
             while (currentPage.Links.Count != 0)
             {
+                // Display the current page description.
                 Console.WriteLine(currentPage.Description);
+
+                // Transition to the next page based on user input.
                 currentPage = GetSelectedPage(currentPage.Links);
+
+                // Return false if the user chose to quit.
+                if (currentPage == null)
+                    return false;
             }
 
+            // Display the ending page description.
             Console.WriteLine(currentPage.Description);
+            return true;
+        }
 
-            Console.WriteLine("\nTHE END - Press ENTER to exit.");
-            Console.ReadLine();
+        static bool AskYesNo()
+        {
+            for (; ; )
+            {
+                switch (Console.ReadKey().KeyChar)
+                {
+                    case 'y':
+                        Console.WriteLine();
+                        return true;
+
+                    case 'n':
+                        Console.WriteLine();
+                        return false;
+                }
+            }
         }
 
         static Page GetSelectedPage(List<Link> links)
         {
+            // Automatically advance if there's only one link.
             if (links.Count == 1)
             {
                 return links[0].Target;
             }
 
+            // Display the menu.
             Console.WriteLine("You can:");
             for (int i = 0; i < links.Count; ++i)
             {
                 Console.WriteLine($" {i + 1}. {links[i].Text}");
             }
-
             Console.Write("> ");
 
+            // Loop indefinitely until the user presses a valid key.
             for (; ; )
             {
                 char key = Console.ReadKey().KeyChar;
+
                 if (key >= '1' && key <= '9')
                 {
+                    // It's a digit, so convert it to a zero-based index.
+                    // If the index is in range, return the link target.
                     int index = key - '1';
                     if (index < links.Count)
                     {
                         Console.WriteLine("\n\n------\n");
                         return links[index].Target;
                     }
+                }
+                else if (key == 'q')
+                {
+                    // The user pressed the quit key, so return null.
+                    Console.WriteLine();
+                    return null;
                 }
             }
         }
