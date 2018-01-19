4b5f48f6fc6e5d8824fecb213ea2d18d14a266aa
Michaeljpm <hopperboy1@gmail.com>
2018-01-18 16:10:38 -0800

    add adventure program

---
+++ Michael/ConsoleApp1/ConsoleApp1/Page.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Page
+    {
+        public Page(string description)
+        {
+            Description = description;
+            Links = new List<Link>();
+        }
+        public void AddLink(string text, Page destination)
+        {
+            Links.Add(new Link(text,destination));
+        }
+        public string Description { get; }
+        public List<Link> Links { get; }
+    }
+}
---
+++ Michael/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentPage = CreatePages();
+            while (currentPage.Links.Count != 0)
+            {
+                Console.WriteLine(currentPage.Description);
+                DisplayMenu(currentPage.Links);
+                currentPage = GetMenuSelection(currentPage.Links);
+            }
+            Console.WriteLine(currentPage.Description);
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Creates all the pages and links in the story and returns the reference to the startpage
+        /// </summary>
+        static Page CreatePages()
+        {
+            var startPage = new Page("You are outside a scary house...");
+            var homePage = new Page("You go home and survive");
+            var foyerPage = new Page("You enter the foyer, its dark and can bearly see");
+            var kitchenPage = new Page("While opening the refrigerator, it falls on you and you die");
+            var mainRoomPage = new Page("The old floor gives out under you and you fall down an endless pit");
+            startPage.AddLink("go home", homePage);
+            startPage.AddLink("enter the house", foyerPage);
+            foyerPage.AddLink("Leave house", startPage);
+            foyerPage.AddLink("enter kitchen", kitchenPage);
+            foyerPage.AddLink("enter the main room", mainRoomPage);
+            return startPage;
+        }
+        static void DisplayMenu(List<Link> links)
+        {
+            Console.WriteLine("What do you do?");
+            for (int i = 0; i < links.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {links[i].Text}");
+            }
+        }
+        static Page GetMenuSelection(List<Link> links)
+        {
+            for(; ; )
+            {
+                char inputChar = Console.ReadKey().KeyChar;
+                if (inputChar >= '1' && inputChar <= '9')
+                {
+                    int index = inputChar - '1';
+                    if (index < links.Count)
+                        return links[index].Destination;
+                }
+            }
+        }
+    }
+}
