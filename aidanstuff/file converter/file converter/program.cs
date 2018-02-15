5a25c85556db565d953f789f1a0dc895bbda33b0
Aidan Dona <zaldimmar@outlook.com>
2018-02-14 20:26:00 -0800

    fixed

--- aidanstuff/file converter/file converter/program.cs
+++ aidanstuff/file converter/file converter/program.cs
@@ -24,6 +24,7 @@ namespace File_Converter
 
         public static void InputSelection(string[] input)
         {
+            FileOptions newFO = new FileOptions();
             switch (input[0])
             {
                 case "dir":
@@ -37,10 +38,10 @@ namespace File_Converter
                     FileOptions.Kringle(input[1]);
                     break;
                 case "mv":
-                    FileOptions.ChangeFileProperties(2);
+                    newFO.ChangeFileProperties(2);
                     break;
                 case "c":
-                    FileOptions.ChangeFileProperties(1);
+                    newFO.ChangeFileProperties(1);
                     break;
                 case "html":
                     //specifying File_Converter Namespace because Convert.cs is also the name of a System.IO class
