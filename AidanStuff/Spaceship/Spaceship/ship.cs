7926ac4bfcbf22ba3ed8dfedfd0c08352963edfd
Zaldimmar <PyroFire12@gmail.com>
2018-03-20 14:30:25 -0700

    ahhhh

---
+++ AidanStuff/Spaceship/Spaceship/ship.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Brushes;
+using Microsoft.Graphics.Canvas.Geometry;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+
+namespace Spaceship
+{
+    class Ship : MovingSpaceObject, SpaceResource
+    {
+        CanvasGeometry sGeometry;
+        ICanvasImage sImage;
+        ICanvasImage thrustImage;
+
+        const float PI = (float)Math.PI;
+        const float rSpeed = 0.75f * PI * 2;
+        const float Accel = 50;
+
+        float shipAngle = 0;
+
+        public bool isRotatingLeft { get; set; }
+        public bool isRotatingRight { get; set; }
+        public bool isThrusting { get; set; }
+
+        public override CanvasGeometry Geometry => sGeometry;
+        public override Matrix3x2 WorldTransform => Matrix3x2.CreateRotation(shipAngle) * base.WorldTransform;
+
+
+
+        public void CreateResources(ICanvasResourceCreator resourceCreator)
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            sGeometry?.Dispose();
+            sGeometry = null;
+            sImage?.Dispose();
+            sImage = null;
+        }
+
+        public override void Draw(CanvasDrawingSession drawingSession)
+        {
+            drawingSession.DrawImage(sImage);
+        }
+    }
+}
---
+++ Life/Life/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Life
+{
+    class Program
+    {
+        public static int w = Console.LargestWindowWidth / 2, h = Console.LargestWindowHeight / 2;
+        public static char cell = (char)183;
+        public static int[] Neighbours;
+        
+
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(w, h);
+            Console.SetWindowPosition(0, 0);
+            Console.CursorVisible = false;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+
+
+            Neighbours = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            //TopLeft Top TopRight Left Right  BottomLeft Bottom BottomRight
+
+            int x = 0;
+            Console.SetCursorPosition((Console.WindowWidth / 2), (Console.WindowHeight / 2));
+            Console.Write(cell);
+
+            while (true)
+            {
+                Draw(x);
+                Console.Read();
+                x++;
+            }
+            
+        }
+        public static void Draw(int it)
+        {
+            
+                Console.SetCursorPosition((Console.WindowWidth / 2)-it, (Console.WindowHeight / 2));
+                Console.Write(cell);
+            
+        }
+    }
+}
