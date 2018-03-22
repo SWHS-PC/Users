6920790320f856b27fbbf81fb9ac89bd8b169fde
Zaldimmar <PyroFire12@gmail.com>
2018-03-22 14:30:24 -0700

    done

---
+++ AidanStuff/LifeGui/LifeGui/DrawForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿using System.Drawing;
+
+namespace LifeGui
+{
+    partial class DrawForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.BackColor = System.Drawing.Color.White;
+            this.PictureBox1.Location = new System.Drawing.Point(50, 50);
+            this.PictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(755, 255);
+            this.PictureBox1.TabIndex = 0;
+            this.PictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.DrawClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(131, 345);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.DrawNext);
+            // 
+            // DrawForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(859, 411);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PictureBox1);
+            this.Name = "DrawForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.DrawForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ AidanStuff/LifeGui/LifeGui/DrawForm.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LifeGui
+{
+    public partial class DrawForm : Form
+    {
+        System.Drawing.Graphics drawGraphics;
+        System.Drawing.Pen drawPenBlack = new System.Drawing.Pen(Color.Black, 0.5F);
+        System.Drawing.SolidBrush drawBrushBlack = new System.Drawing.SolidBrush(Color.Black);
+        System.Drawing.SolidBrush drawBrushWhite = new System.Drawing.SolidBrush(Color.White);
+
+        public static int w = 75, h = 25;
+        public static bool[,] Map;
+        public static bool[,] newMap;
+        public bool this[int x, int y]
+        {
+            get { return Map[x, y]; }
+            set { Map[x, y] = value; }
+        }
+        public bool clicked;
+
+
+        public DrawForm()
+        {
+            InitializeComponent();
+
+            Map = new bool[w, h];
+            newMap = new bool[w, h];
+            AddCell(w / 2 - 1, h / 2);
+            AddCell(w / 2, h / 2);
+            AddCell(w / 2 + 1, h / 2);
+            AddCell(w / 2, h / 2 - 1);
+            AddCell(w / 2 - 1, h / 2 + 1);
+            AddCell(w / 2 + 1, h / 2 + 1);
+            AddCell(w / 2, h / 2 + 2);
+            
+        }
+
+        public void NextGen()
+        {
+            newMap = new bool[w, h];
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    int numberOfNeighbors = IsNeighbor(Map, i, j, -1, 0)
+                     + IsNeighbor(Map, i, j, -1, 1)
+                     + IsNeighbor(Map, i, j, 0, 1)
+                     + IsNeighbor(Map, i, j, 1, 1)
+                     + IsNeighbor(Map, i, j, 1, 0)
+                     + IsNeighbor(Map, i, j, 1, -1)
+                     + IsNeighbor(Map, i, j, 0, -1)
+                     + IsNeighbor(Map, i, j, -1, -1);
+
+                    bool shouldLive = false;
+                    bool isAlive = Map[i, j];
+
+                    if (isAlive && (numberOfNeighbors == 2 || numberOfNeighbors == 3))
+                    {
+                        shouldLive = true;
+                    }
+                    else if (!isAlive && numberOfNeighbors == 3)
+                    {
+                        shouldLive = true;
+                    }
+
+                    newMap[i, j] = shouldLive;
+                }
+            }
+            Map = newMap;
+        }
+
+        static int IsNeighbor(bool[,] Map, int x, int y, int offsetx, int offsety)
+        {
+            int result = 0;
+            int proposedOffsetX = x + offsetx;
+            int proposedOffsetY = y + offsety;
+            bool outOfBounds = proposedOffsetX < 0 || proposedOffsetX >= w | proposedOffsetY < 0 || proposedOffsetY >= h;
+            if (!outOfBounds)
+            {
+                result = Map[x + offsetx, y + offsety] ? 1 : 0;
+            }
+            return result;
+        }
+
+        public bool AddCell(int x, int y)
+        {
+            bool currentValue = Map[x, y];
+            return Map[x, y] = !currentValue;
+        }
+
+        private void DrawForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void DrawClick(object sender, EventArgs e)
+        {
+            drawGraphics = PictureBox1.CreateGraphics();
+
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    drawGraphics.DrawRectangle(drawPenBlack, x * 10, y * 10, 10, 10);
+                }
+            }
+        }
+
+        
+
+        private void DrawNext(object sender, EventArgs e)
+        {
+            Draw();
+            NextGen();
+        }
+
+        private void Draw()
+        {
+            drawGraphics = PictureBox1.CreateGraphics();
+            
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    drawGraphics.FillRectangle(Map[x, y] ? drawBrushBlack : drawBrushWhite, (x * 10) + 1, (y * 10) + 1, 9, 9);
+                }
+            }
+        }
+    }
+}
