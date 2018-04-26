98b9893f569c5805bf84c8eadb4c07da18f212e3
Niklas Borson <neborson@outlook.com>
2018-04-26 10:51:36 -0700

    Add more comments to Num parser.

--- NiklasB/num/Parser.h
+++ NiklasB/num/Parser.h
@@ -18,22 +18,49 @@ public:
 
     /*
     Grammar:
-        expression :            binary_expression ( "?" binary_expression ":" expression )?
-        binary_expression :     unary_expression ( BINARY_OP unary_expression )*
-        unary_expression :      PREFIX_OP* ( simple_expression POSTFIX_OP* )
-        simple_expression :     NUMBER | group | variable_or_function
-        group :                 "(" expression ")"
-        variable_or_function :  NAME argument_list?
-        argument_list :         "(" ( expression ( "," expression )* )? ")"
+
+        expression              =>  binary_expression ( "?" binary_expression ":" expression )?
+        binary_expression       =>  unary_expression ( BINARY_OP unary_expression )*
+        unary_expression        =>  PREFIX_OP* ( simple_expression POSTFIX_OP* )
+        simple_expression       =>  NUMBER | group | variable_or_function
+        group                   =>  "(" expression ")"
+        variable_or_function    =>  NAME argument_list?
+        argument_list           =>  "(" ( expression ( "," expression )* )? ")"
+
+        Note that the grammer for binary_expression does not take into account operator
+        precedence. The ParseBinaryExpression method uses the precedence climbing method
+        to construct an expression tree that, for example, correctly distinguishes 
+        between the following two cases:
+
+        Expression     Syntax Tree
+
+        5 * 2 + 3           +
+                           / \
+                          *   3
+                         / \
+                        5   2
+
+        5 + 2 * 3           +
+                           / \
+                          5   *
+                             / \
+                            2   3
     */
 
+    // ParseFullExpression parses an expression and verifies that it is not followed
+    // by any additional tokens.
     ExpressionPtr ParseFullExpression();
 
+    // ParseExpression parses an expression according to the above grammar. The
+    // expression may be followed by additional tokens, as in the case where it's
+    // part of a larger expression.
     ExpressionPtr ParseExpression();
 
 private:
+    // Helper method to throw a parser exception.
     void Fail(char const* message = "Syntax error.") const;
 
+    // Methods for parsing elements of the grammer.
     ExpressionPtr ParseBinaryExpression();
     ExpressionPtr ParseBinaryExpression(ExpressionPtr&& leftExpr, int minPrecedence);
     ExpressionPtr ParseUnaryExpression();
@@ -41,7 +68,13 @@ private:
     ExpressionPtr ParseVariableOrFunction();
     ExpressionPtr ParseFunction(DefinitionPtr def);
 
+    // Lexer used to read input tokens.
     Lexer& lexer_;
+
+    // Global name map used to resolve variable and parameter names.
     NameMap const& globals_;
+
+    // Definition pointer used to resolve parameter names if the current
+    // expression is part of a function definition; can be null.
     DefinitionPtr currentDefinition_;
 };
