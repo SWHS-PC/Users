6a38dd04d2dc6de879dd97d41262555a63c22a03
Zaldimmar <PyroFire12@gmail.com>
2018-05-24 16:03:58 -0700

    asdad

---
+++ AidanStuff/Generics/Generics/List.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Generics
+{
+    class List<T> : IEnumerable<T>
+    {
+        T[] items;
+        int count;
+
+        public int Count => count;
+
+        public T this[int i]
+        {
+            get { return items[i]; }
+            set { items[i] = value; }
+        }
+        
+
+        public void Add(T item)
+        {
+            EnsureCapacity(count + 1);
+            items[count++] = item;
+        }
+
+        void EnsureCapacity(int minCapacity)
+        {
+            if(items == null)
+            {
+                int c = Math.Max(8, minCapacity);
+                items = new T[c];
+            }
+            else if(items.Length < minCapacity)
+            {
+                int c = Math.Max(items.Length * 2, minCapacity);
+                var newItems = new T[c];
+                Array.Copy(items, newItems, count);
+                items = newItems;
+            }
+            else
+            {
+
+            }
+        }
+
+        class Enumerator : IEnumerator<T>
+        {
+            T[] _items;
+            int _count;
+            int _index = -1;
+
+            public Enumerator(T[] items, int count)
+            {
+                _items = items;
+                _count = count;
+            }
+
+            public T Current => _items[_index];
+
+            object IEnumerator.Current => _items[_index];
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                if(_index < _count)
+                {
+                    ++_index;
+                }
+                return _index < _count;
+            }
+
+            public void Reset()
+            {
+                _index = -1;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Enumerator(items, count);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new Enumerator(items, count);
+        }
+    }
+}
---
+++ AidanStuff/Generics/Generics/Queue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Queue<T>
+    {
+        T[] items;
+        int count;
+        int firstIndex;
+
+        int ArrayIndex(int i) => (firstIndex + i) & (items.Length - 1);
+
+        public int Count => count;
+        public T Front => items[firstIndex];
+        public T this[int i] => items[ArrayIndex(i)];
+
+        public void Push(T item)
+        {
+            if(items == null)
+            {
+                items = new T[8];
+            }
+            else if(items.Length == count)
+            {
+                var newItems = new T[items.Length * 2];
+
+                for(int i = 0; i < count; i++)
+                {
+                    newItems[i] = this[i];
+                }
+
+                items = newItems;
+                firstIndex = 0;
+            }
+
+            items[ArrayIndex(count++)] = item;
+        }
+        public void Pop()
+        {
+            if(count == 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            items[firstIndex] = default(T);
+
+            firstIndex = ArrayIndex(1);
+            count--;
+        }
+    }
+}
