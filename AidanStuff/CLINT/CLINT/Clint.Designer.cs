a6dd7f82b277365ca8fd6ea2123f503a96e2afa9
Aidan Dona <zaldimmar@outlook.com>
2018-06-12 22:44:54 -0700

    I made a program with some commands but one of the comamnds "rekt"  opens all present cd drives on computer and closes them repeatedly

---
+++ AidanStuff/CLINT/CLINT/Clint.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CLINT
+{
+    partial class Clint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.AcceptsReturn = true;
+            this.Input.BackColor = System.Drawing.Color.White;
+            this.Input.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Input.Location = new System.Drawing.Point(12, 12);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(288, 13);
+            this.Input.TabIndex = 0;
+            this.Input.WordWrap = false;
+            this.Input.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Run);
+            // 
+            // Clint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(312, 38);
+            this.Controls.Add(this.Input);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Clint";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox Input;
+    }
+}
+
---
+++ AidanStuff/Shell/Shell/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shell
+{
+    class Program
+    {
+        [DllImport("winmm.dll", EntryPoint = "mciSendString")]
+        public static extern int mciSendStringA(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+        public static string file;
+        public static string[] driveLetter = {"E", "F", "G"};
+        
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a command");
+                string a = Console.ReadLine();
+                if (a == "run")
+                {
+                    file = Console.ReadLine();
+                    string dir = Console.ReadLine(); 
+                    string arg = Console.ReadLine();
+                    Process start = new Process();
+                    start.StartInfo.FileName = file;
+                    start.StartInfo.WorkingDirectory = dir;
+                    start.StartInfo.Arguments = arg;
+                    //start.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    start.Start();
+                }
+
+                if (a == "kill")
+                {
+                    try
+                    {
+                        Process[] proc = Process.GetProcessesByName(file);
+                        foreach (Process x in proc)
+                        {
+                            Console.WriteLine(x);
+                        }
+                        Console.WriteLine("Kill Process?(y/n)");
+                        string kill = Console.ReadLine();
+                        if (kill == "y")
+                        {
+                            proc[0].Kill();
+                        }
+                    }
+                    catch { }
+                }
+
+                if(a == "rekt")
+                {
+                    while (true)
+                    {
+                        foreach (string x in driveLetter)
+                        {
+                            mciSendStringA("open " + x + ": type CDaudio alias drive" + x, "", 0, 0);
+                            mciSendStringA("set drive" + x + " door open", "", 0, 0);
+
+                            mciSendStringA("open " + x + ": type CDaudio alias drive" + x, "", 0, 0);
+                            mciSendStringA("set drive" + x + " door closed", "", 0, 0);
+                        }
+                    }
+                }
+            }
+        }
+
+        
+    }
+}
+
+
+//start.StartInfo.FileName = @"cscript";
+//start.StartInfo.WorkingDirectory = @"c:\";
+//start.StartInfo.Arguments = " //B //Nologo a.vbs";
+
+//Process.Start("C:\\a.exe");
+//ProcessStartInfo start = new ProcessStartInfo();
+//start.FileName = "C:\\a.exe";
+//Process.Start(start);
+
+//Process.Start(@"cscript //B //Nologo c:\scripts\vbscript.vbs");
